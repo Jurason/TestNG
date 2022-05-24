a113d6ba938f9f26f283f3209e6b5bb4dd85a944
Jurason <evgenilymanyuk@gmail.com>
2022-05-24 08:42:42 +0300

    Add parametr

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -1,12 +1,14 @@
 package smoke;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
+    @Parameters({"Parametr1"})
     @Test (groups = {"SmokeTest"})
-    public void test1 () {
-        System.out.print("test1");
+    public void test1 (String Parametr1) {
+        System.out.print("test1" + Parametr1);
     }
 
     @Test (groups = {"Regression"})
