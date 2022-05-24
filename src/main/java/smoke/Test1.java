ca7940c9e3b41fdd4c025d42883e044115ff89d2
Jurason <evgenilymanyuk@gmail.com>
2022-05-24 17:53:41 +0300

    data provider

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -8,17 +8,17 @@ public class Test1 {
     @Parameters({"Parametr1"})
     @Test (groups = {"SmokeTest"})
     public void test1 (String Parametr1) {
-        System.out.print("test1" + Parametr1);
+        System.out.println("test1" + Parametr1);
     }
 
     @Test (groups = {"Regression"})
     public void test11 () {
-        System.out.print("test11");
+        System.out.println("test11");
     }
 
     @Test (groups = {"Regression"})
     public void test111 () {
-        System.out.print("test111");
+        System.out.println("test111");
     }
     
 }
